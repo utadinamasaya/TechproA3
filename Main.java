6c3ac701de0c9f8632cd672bd3b8e4470fc615af
utakendu <utakendu@gmail.com>
2025-08-23 13:00:24 +0000

    just some basic stuff for the project nothing much

--- Main.java
+++ Main.java
@@ -1,1 +1,14 @@
-
+public class Main {
+    public static void main (String[]args) {
+   System.out.println("SEATWORKFIVE");
+       
+           int countdown=3;
+               
+                   while (countdown > 0) {
+                         System.out.println(countdown);
+                               countdown--;
+                                     
+                                         }
+                                             System.out.println("HAPPY NEW YEAR!!");
+                                               }
+  } 
---
+++ Main2.java
@@ -0,0 +1,15 @@
+public class Main2 {
+   public static void main (String[]args) {
+    System.out.println("SEATWORKSIX");
+        int dice = 1;
+            
+                while (dice < 6 ){
+                     if (dice < 6 ){
+                             System.out.println("loves me not");
+                                   }else {
+                                           System.out.println("loves me");
+                                                 }
+                                                       dice = dice + 1;
+     }            
+    }     
+}
