db20cef876fcd7018f21900d4f2fabad4c8bad12
utakendu <utakendu@gmail.com>
2025-08-23 13:07:08 +0000

    rjirjrjrjr

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main (String[]args) {
    System.out.println("SEATWORKFIVE");
        
-           int countdown=3;
+        int countdown=3;
                
                    while (countdown > 0) {
                          System.out.println(countdown);
