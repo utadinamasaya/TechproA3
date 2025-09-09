e896b581e39a42173ad1dc3760f6c82f5239dbee
utakendu <utakendu@gmail.com>
2025-09-09 12:10:35 +0000

    haha code go brrrr sir bat ayaw mo ako paniwalaan gumana code ko

---
+++ SeatworkEleven.java
@@ -0,0 +1,17 @@
+public class SeatworkEleven {
+	public static void main(String[] args) {
+	System.out.println("SEATWORK11PART1");
+	int[][] myNumbers = { {1, 4, 2}, {3, 6, 8} };
+	myNumbers [1][2] = 9;
+	System.out.println(myNumbers[1][2]);
+		System.out.println("SEATWORKELEVENPART2");
+				int [][] mynumbers={ {1,4,2},{3,6,8,5,2} };
+				    for(int row=0; row<mynumbers.length; row++){
+					      for (int col=0;col < mynumbers[row].length; col++) {
+						          System.out.println("mynumbers["+row+"]" + "["+col+"]="+mynumbers[row][col]);
+								        }
+										      
+											      }
+	}
+}
+
